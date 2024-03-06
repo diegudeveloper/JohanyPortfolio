1747ee283e1ae6b71221a34ef065cf6ed22e4efa
Diegu <diegudeveloper@gmail.com>
2024-03-06 20:50:48 +0100

    Inicio de la configuracion del swicher

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,8 +52,19 @@ function scrollHeader() {
 
 window.addEventListener('scroll', scrollHeader);
 
+/*=============== Style Switcher =============== */
+const styleSwitcherToggle = document.querySelector('.style__switcher-toggler'),
+styleSwitcher = document.querySelector('.style__switcher');
 
+styleSwitcherToggle.addEventListener('click', () => {
+    styleSwitcher.classList.toggle('open');
+});
 
-/*=============== Active Link =============== */
+//  hide switcher on scroll 
+window.addEventListener('scroll', () => {
+    if (styleSwitcher.classList.contains('open')) {
+        styleSwitcher.classList.remove('open');
+    }
+})
 
 /*=============== Mixitup Filter =============== */
